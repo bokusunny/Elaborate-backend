2e6d5fe2034120e6024e9af1c73c137888120821
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-05-22 19:09:14 +0900

    Successfully save data on DB

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type User struct {
+	ID        int       `json:"id" gorm:"primary_key auto_increment"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 func gormConnect() *gorm.DB {
 	// TODO: 環境変数に置き換える
@@ -24,4 +36,18 @@ func gormConnect() *gorm.DB {
 func main() {
 	db := gormConnect()
 	defer db.Close()
+
+	userExample := User{}
+	userExample.Name = "elaborate"
+	userExample.Email = "elaborate@example.com"
+	userExample.CreatedAt = time.Now()
+	userExample.UpdatedAt = time.Now()
+	marshalUser, err := json.Marshal(userExample)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("thrown json:", string(marshalUser))
+	db.Create(&userExample)
 }
