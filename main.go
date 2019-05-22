ecac229f9b780b3851c8518c7d7c38ee627b13c5
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-05-23 06:42:11 +0900

    Rebuild http server

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -31,6 +34,20 @@ func gormConnect() *gorm.DB {
 	return db
 }
 
+func postUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		}
+
+		log.Printf("I got post request, json: " + string(body))
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	db := gormConnect()
 	defer db.Close()
@@ -40,4 +57,7 @@ func main() {
 	}
 
 	db.AutoMigrate(&User{})
+
+	http.HandleFunc("/", postUserHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
